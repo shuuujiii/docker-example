df1f0b1aef8a9fa6a8cca96d330d5e0fe920028d
shuji watanabe <rinkioutouhenmonzizai@yahoo.co.jp>
2021-06-12 00:57:18 +0900

    add mongodb docker

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const app = express();
-
+// server.js
+mongoose.connect(
+    'mongodb://nabeshi:mypassword@mongo:27017/?authSource=admin',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => { console.log('successfully connected to DB') })
+    .catch(err => { console.log(err) })
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
