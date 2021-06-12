85547799c42405b7043e546ecf98e78c82f1fed9
shuji watanabe <rinkioutouhenmonzizai@yahoo.co.jp>
2021-06-12 17:34:45 +0900

    add signin

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,12 @@
 // authController.js
 const User = require('../models/userModel')
+const bcrypt = require('bcryptjs')
 
 exports.signUp = async (req, res, next) => {
+    const { username, password } = req.body
+    const hashedpassword = await bcrypt.hash(password, 12)
     try {
-        const newUser = await User.create(req.body)
+        const newUser = await User.create({ username: username, password: hashedpassword })
         res.status(200).json({
             status: 'success',
             data: {
@@ -14,6 +17,38 @@ exports.signUp = async (req, res, next) => {
         console.log(e)
         res.status(400).json({
             status: 'fail'
+        })
+    }
+}
+
+exports.signIn = async (req, res, next) => {
+    try {
+        const { username, password } = req.body
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'user not found',
+            })
+        }
+        const isCorrectPass = await bcrypt.compare(password, user.password)
+        if (!isCorrectPass) {
+            return res.status(400).json({
+                status: 'fail',
+                message: 'incorrect username or password',
+            })
+        }
+        res.status(200).json({
+            status: 'success',
+            data: {
+                user: user,
+            }
+        })
+
+    } catch (e) {
+        console.log(e)
+        res.status(400).json({
+            status: 'fail',
         })
     }
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,6 @@ const authController = require("../controllers/authController")
 
 const router = express.Router()
 
-router.route("/")
-    .post(authController.signUp)
+router.route("/").post(authController.signUp)
+router.route("/login").post(authController.signIn)
 module.exports = router
